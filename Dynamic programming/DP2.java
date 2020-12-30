317e04ad71ddb3ba65ec1a370e19ee950da7517c
shangde <shangdehan@gmail.com>
2020-12-30 09:10:38 -0800

    Add files via upload

--- Dynamic programming/DP2.java
+++ Dynamic programming/DP2.java
@@ -15,4 +15,25 @@ public class DP2 {
         }
         return max;
     }
+
+    /*
+        LC Climbing Stairs
+        Input: n = 2
+        Output: 2
+        Explanation: There are two ways to climb to the top.
+        1. 1 step + 1 step      2. 2 steps
+     */
+    public int climbStairs(int n) {
+        if(n == 0)return 0;
+        if(n == 1)return 1;
+        if(n == 2)return 2;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
 }

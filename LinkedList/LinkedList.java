45f87fb645eaff607bf86b9ce752542090a95924
ShangdeHan <shangdehan@gmail.com>
2021-03-22 19:16:55 -0700

    Update LinkedList.java

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -52,9 +52,6 @@ public class LinkedList {
     Input: head = [1,1,2,3,3]
     Output: [1,2,3]
  */
-
-
-
     public ListNode deleteDuplicates(ListNode head) {
         ListNode n = head;
         while(n!= null && n.next != null){
@@ -65,5 +62,46 @@ public class LinkedList {
             }
         }
         return head;
+    }
+
+    /*
+    82. Remove Duplicates from Sorted List II
+    Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+    Return the linked list sorted as well.
+    Example1:
+    Input: head = [1,2,3,3,4,4,5]
+    Output: [1,2,5]
+    Example2:
+    Input: head = [1,1,1,2,3]
+    Output: [2,3]
+     */
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        // sentinel
+        ListNode sentinel = new ListNode(0, head);
+
+        // predecessor = the last node
+        // before the sublist of duplicates
+        ListNode pred = sentinel;
+
+        while (head != null) {
+            // if it's a beginning of duplicates sublist
+            // skip all duplicates
+            if (head.next != null && head.val == head.next.val) {
+                // move till the end of duplicates sublist
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+                // skip all duplicates
+                pred.next = head.next;
+                // otherwise, move predecessor
+            } else {
+                pred = pred.next;
+            }
+
+            // move forward
+            head = head.next;
+        }
+        return sentinel.next;
     }
 }

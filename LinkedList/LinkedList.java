d72f275b7beff31c97ef26699640a91ec63c8685
ShangdeHan <shangdehan@gmail.com>
2021-03-30 21:46:27 -0700

    Update LinkedList.java

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -180,4 +180,48 @@ public class LinkedList {
         head = current;
         return head;
     }
+
+
+    /*
+        2. Add Two Numbers
+        You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+        and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        Example 1:
+        Input: l1 = [2,4,3], l2 = [5,6,4]
+        Output: [7,0,8]
+        Explanation: 342 + 465 = 807.
+
+        Example 2:
+        Input: l1 = [0], l2 = [0]
+        Output: [0]
+
+        Example 3:
+        Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        Output: [8,9,9,9,0,0,0,1]
+     */
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode();
+        ListNode l4 = l3;
+        ListNode l5;
+        int next = 0;
+        while(l1 !=null || l2 != null){
+            int num1 = (l1 != null) ? l1.val : 0;
+            int num2 = (l2 != null) ? l2.val : 0;
+            if(num1 + num2 + next > 9){
+                l5 = new ListNode(num1 + num2 + next - 10);
+                next = 1;
+            }else{
+                l5 = new ListNode(num1 + num2 + next);
+                next = 0;
+            }
+            l3.next = l5;
+            l3 = l3.next;
+            if(l1 != null)l1 = l1.next;
+            if(l2 != null)l2 = l2.next;
+        }
+        if(next == 1) l3.next = new ListNode(1);
+        return l4.next;
+    }
 }
